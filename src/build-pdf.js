37638525b380346fb0e24a76cbf27ae4143c47dc
Hossein Margani <hossein@margani.dev>
2024-06-23 09:33:20 +0100

    Add build pdf task

---
+++ src/build-pdf.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+(async function () {
+  const content = fs.readFileSync("dist/index.html", "utf8");
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.setContent(content);
+  await page.evaluateHandle("document.fonts.ready");
+
+  const pdfContents = await page.pdf({ format: "A4" });
+  await browser.close();
+
+  fs.writeFileSync("dist/cv.pdf", pdfContents);
+})();
